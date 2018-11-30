a2aba5f2f622d838aea956680a40980a6510731d
Nicolas R <atoomic@cpan.org>
2018-11-30 13:28:35 -0700

    add chop + unpack

--- split.pl
+++ split.pl
@@ -9,64 +9,97 @@ use Benchmark qw /timethese cmpthese/;
 note "Perl $]";
 
 our $char;
+our $S;
 
 for my $size ( 4, 16, 64, 256, 1024 ) {
     note "Iterating over a string of length $size.\n";
 
-    our $S = join '', map { chr( int rand(255) ) } 1 .. $size;
+    $S = join '', map { chr( int rand(255) ) } 1 .. $size;
     die "Incorrect str length $size" unless length $S == $size;
 
     cmpthese - 5 => {
         split => sub {
             my $str = $S;
             for my $c ( split // => $str ) { $char = $c }
+
+            return;
         },
         for => sub {
             my $str = $S;
             for my $c ( $str =~ /(.)/g ) { $char = $c }
+
+            return;
         },
         while => sub {
             my $str = $S;
             while ( $str =~ /(.)/g ) { $char = $1 }
+
+            return;
         },
         substr => sub {
             my $str = $S;
             my $len = length($str) - 1;
             for my $i ( 0 .. $len ) { $char = substr $str, $i, 1 }
+
+            return;
         },
+        chop => sub {
+            my $str = $S;
+            my $len = length($str) - 1;
+            for my $i ( 0 .. $len ) { $char = chop $str }
+
+            return;
+        },
+        unpack => \&using_unpack,
     };
 }
 
+sub using_unpack {
+    my $str = $S;
+    my @A = unpack '(A)*', $S;
+
+    return;
+}
 __END__
 
 # Perl 5.026002
 # Iterating over a string of length 4.
-            Rate    for  while  split substr
-for     760839/s     --    -8%   -48%   -71%
-while   830451/s     9%     --   -44%   -68%
-split  1470358/s    93%    77%     --   -43%
-substr 2602040/s   242%   213%    77%     --
+            Rate    for  while  split unpack substr   chop
+for     745114/s     --    -9%   -47%   -48%   -71%   -74%
+while   817702/s    10%     --   -42%   -43%   -68%   -71%
+split  1413399/s    90%    73%     --    -1%   -45%   -51%
+unpack 1432269/s    92%    75%     1%     --   -44%   -50%
+substr 2548620/s   242%   212%    80%    78%     --   -11%
+chop   2868839/s   285%   251%   103%   100%    13%     --
 # Iterating over a string of length 16.
-           Rate    for  while  split substr
-for    213412/s     --   -21%   -48%   -77%
-while  269588/s    26%     --   -34%   -71%
-split  410738/s    92%    52%     --   -55%
-substr 914574/s   329%   239%   123%     --
+            Rate    for  while  split unpack substr   chop
+for     212789/s     --   -19%   -47%   -49%   -77%   -81%
+while   261649/s    23%     --   -34%   -38%   -71%   -76%
+split   398898/s    87%    52%     --    -5%   -56%   -64%
+unpack  420763/s    98%    61%     5%     --   -54%   -62%
+substr  911667/s   328%   248%   129%   117%     --   -18%
+chop   1111687/s   422%   325%   179%   164%    22%     --
 # Iterating over a string of length 64.
-           Rate    for  while  split substr
-for     53588/s     --   -23%   -48%   -79%
-while   69553/s    30%     --   -32%   -73%
-split  102603/s    91%    48%     --   -60%
-substr 259172/s   384%   273%   153%     --
+           Rate    for  while  split unpack substr   chop
+for     53605/s     --   -23%   -47%   -50%   -79%   -83%
+while   69421/s    30%     --   -32%   -36%   -73%   -78%
+split  101820/s    90%    47%     --    -6%   -61%   -68%
+unpack 107990/s   101%    56%     6%     --   -58%   -66%
+substr 257964/s   381%   272%   153%   139%     --   -18%
+chop   315248/s   488%   354%   210%   192%    22%     --
 # Iterating over a string of length 256.
-          Rate    for  while  split substr
-for    13471/s     --   -23%   -47%   -80%
-while  17570/s    30%     --   -31%   -74%
-split  25316/s    88%    44%     --   -62%
-substr 67111/s   398%   282%   165%     --
+          Rate    for  while  split unpack substr   chop
+for    13295/s     --   -24%   -47%   -51%   -80%   -83%
+while  17471/s    31%     --   -30%   -35%   -74%   -78%
+split  25119/s    89%    44%     --    -7%   -63%   -68%
+unpack 27052/s   103%    55%     8%     --   -60%   -66%
+substr 67265/s   406%   285%   168%   149%     --   -15%
+chop   79480/s   498%   355%   216%   194%    18%     --
 # Iterating over a string of length 1024.
-          Rate    for  while  split substr
-for     3375/s     --   -22%   -47%   -80%
-while   4322/s    28%     --   -32%   -74%
-split   6335/s    88%    47%     --   -62%
-substr 16557/s   391%   283%   161%     --
+          Rate    for  while  split unpack substr   chop
+for     3438/s     --   -21%   -47%   -52%   -80%   -83%
+while   4342/s    26%     --   -33%   -39%   -74%   -79%
+split   6487/s    89%    49%     --    -9%   -62%   -68%
+unpack  7150/s   108%    65%    10%     --   -58%   -65%
+substr 16880/s   391%   289%   160%   136%     --   -17%
+chop   20328/s   491%   368%   213%   184%    20%     --
